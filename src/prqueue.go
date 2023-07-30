95008ccdc5e9ba8c8beb38933f9438841f73415c
Marvin Ewald <marvin.e@protonmail.com>
2023-07-30 21:15:53 +0200

    Add signal to notify when queue head changed

--- src/prqueue.go
+++ src/prqueue.go
@@ -50,12 +50,14 @@ type PriorityQueue[T any] struct {
     mu sync.RWMutex
     items Heap[T]
     itemAdded chan struct{}
+    headPriorityChanged chan struct{}
 }
 
 // Returns and initializes a new priority queue. Smaller values have higher priority.
 func NewPriorityQueue[T any]() *PriorityQueue[T] {
     pq := &PriorityQueue[T]{
         itemAdded: make(chan struct{}),
+        headPriorityChanged: make(chan struct{}),
     }
     return pq
 }
@@ -64,14 +66,21 @@ func NewPriorityQueue[T any]() *PriorityQueue[T] {
 func (pq *PriorityQueue[T]) Push(priority PriorityT, data T) {
     pq.mu.Lock()
     defer pq.mu.Unlock()
+
+    var headPrio time.Time
+
+    if (pq.items.Len() > 0) {
+        headPrio = pq.items[0].priority
+    }
+
     heap.Push(&pq.items, Item[T]{priority, data})
 
-    // Broadcast the itemAdded signal by closing the channel and at the same time create a new
-    // channel for the next broadcast.
-    // NOTE: Since WaitForItemAdded() requires a read lock, it is ensured that code cannot access
-    // the itemAdded channel in a closed state.
-    close(pq.itemAdded)
-    pq.itemAdded = make(chan struct{})
+    // Check if priority of first element changed
+    if pq.items.Len() == 1 || pq.items[0].priority != headPrio {
+        pq._TriggerBroadcast(&pq.headPriorityChanged)
+    }
+
+    pq._TriggerBroadcast(&pq.itemAdded)
 }
 
 // Pops the top item from the priority queue. Blocks if the queue is empty.
@@ -79,7 +88,9 @@ func (pq *PriorityQueue[T]) Pop() Item[T] {
     pq.mu.Lock()
     defer pq.mu.Unlock()
     pq._WaitForData(&pq.mu)
-    return heap.Pop(&pq.items).(Item[T])
+    ret := heap.Pop(&pq.items).(Item[T])
+    pq._TriggerBroadcast(&pq.headPriorityChanged)
+    return ret
 }
 
 // Returns the top item from the priority queue but does not remove it. Blocks if the queue is empty.
@@ -107,6 +118,25 @@ func (pq *PriorityQueue[T]) WaitForItemAdded() <-chan struct{} {
     pq.mu.RLock()
     defer pq.mu.RUnlock()
     return pq.itemAdded
+}
+
+
+// Returns a channel that can be listened on to wait until the priority of the topmost item to change.
+func (pq *PriorityQueue[T]) WaitForHeadPriorityChange() <-chan struct{} {
+    pq.mu.RLock()
+    defer pq.mu.RUnlock()
+    return pq.headPriorityChanged
+}
+
+
+func (pq *PriorityQueue[T]) _TriggerBroadcast(c *chan struct{}) {
+    // First make a new channel to prevent other code from accessing a closed channel through
+    // WaitFor*() functions.
+    old := *c
+    *c = make(chan struct{})
+
+    // Afterwards broadcast the signal by closing the channel.
+    close(old)
 }
 
 
